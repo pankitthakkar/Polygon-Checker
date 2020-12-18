c28d076548f85600853a14c0a988caaf8430ed1d
Pankit Thakkar <pthakkar8404@conestogac.on.ca>
2020-12-18 11:49:55 +0530

    Fixed a Bug in triangleSolver and made a few changes

--- PolygonChecker/triangleSolver.c
+++ PolygonChecker/triangleSolver.c
@@ -4,20 +4,33 @@
 #include "triangleSolver.h"
 
 char* analyzeTriangle(int side1, int side2, int side3) {
-	char* result = "";
-	if (side1 <= 0 || side2 <= 0 || side3 <= 0) {
-		result = "Not a triangle";
-	}
-	else if (side1 == side2 && side1 == side3) {
-		result = "Equilateral triangle";
-	}
-	else if ((side1 == side2 && side1 != side3) ||
-		(side1 == side3 && side1 != side2))
-	{
-		result = "Isosceles triangle";
-	}
-	else {
-		result = "Scalene triangle";
-	}
-	return result;
+
+    int a = side1, b = side2, c = side3;
+    char* Equilateral = "Equilateral Triangle";
+    char* Scalene = "Scalene Triangle";
+    char* Isosceles = "Isosceles Triangle";
+    char* NotATriangle = "Not a Triangle";
+
+    if (a + b > c && a + c > b && b + c > a)
+    {
+        if (side1 == side2 && side1 == side3) {
+            printf("\n%s\n\n", Equilateral);
+            return Equilateral;
+        }
+
+        else if ((side1 == side2 && side1 != side3) || (side1 == side3 && side1 != side2)) {
+            printf("\n%s\n\n", Isosceles);
+            return Isosceles;
+        }
+
+        else {
+            printf("\n%s\n\n", Scalene);
+            return Scalene;
+        }
+    }
+
+    else {
+        printf("\n%s\n\n", NotATriangle);
+        return NotATriangle;
+    }
 }
