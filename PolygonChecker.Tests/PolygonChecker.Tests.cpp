58036647c1ce7b51cdf67df40f6a36b2b5d15f73
Pankit Thakkar <pthakkar8404@conestogac.on.ca>
2020-12-18 11:32:26 +0530

    Created Test Project

---
+++ PolygonChecker.Tests/PolygonChecker.Tests.cpp
@@ -0,0 +1,21 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+extern "C" {
+#include "main.h"
+}
+
+extern "C" {
+#include "triangleSolver.h"
+}
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace PolygonCheckerTests
+{
+	TEST_CLASS(PolygonCheckerTests)
+	{
+	public:
+
+	};
+}
