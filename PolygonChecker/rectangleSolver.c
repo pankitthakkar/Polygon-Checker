015d775889a44539f06d92eeb455a3cc37780b38
Arun Kumar Thulaseedharan Pillai <Athulaseedharan0760@conestogac.on.ca>
2020-12-18 12:50:38 +0530

    Added the functionality to detect the points of triangle in any order

--- PolygonChecker/rectangleSolver.c
+++ PolygonChecker/rectangleSolver.c
@@ -43,3 +43,63 @@ int input(int n) {
     scanf("%d", &n);
     return n;
 }
+
+int IsOrthogonal(point a, point b, point c) {
+    return (b.x - a.x) * (b.x - c.x) + (b.y - a.y) * (b.y - c.y) == 0;
+}
+
+int IsRectangle(point a, point b, point c, point d) {
+    return
+        IsOrthogonal(a, b, c) &&
+        IsOrthogonal(b, c, d) &&
+        IsOrthogonal(c, d, a);
+}
+
+int ok(point a, point b, point c, point d) {
+    return IsRectangle(a, b, c, d) ||
+        IsRectangle(b, c, a, d) ||
+        IsRectangle(c, a, b, d);
+}
+
+int makeSides(point p1, point p2, point p3, point p4) {
+    int k = ok(p1, p2, p3, p4);
+
+    if (k) {
+
+        int side1, side2;
+
+        if (p1.y == p2.y) {
+            side1 = abs(p1.x - p2.x);
+        }
+
+        else if (p1.y == p3.y) {
+            side1 = abs(p1.x - p3.x);
+        }
+
+        else if (p1.y == p4.y) {
+            side1 = abs(p1.x - p4.x);
+        }
+
+        if (p1.x == p2.x) {
+            side2 = abs(p1.y - p2.y);
+        }
+
+        else if (p1.x == p3.x) {
+            side2 = abs(p1.y - p3.y);
+        }
+
+        else if (p1.x == p4.x) {
+            side2 = abs(p1.y - p4.y);
+        }
+
+        printf("\nThe entered coordinates form a Rectangle");
+        printf("\n\nArea of the Rectangle is: %d", side1 * side2);
+        printf("\nPerimeter of Rectangle is: %d\n\n", (2 * (side1 + side2)));
+        return 0;
+    }
+
+    else {
+        printf("\n\nSorry, the entered coordinates do not form a rectangle.\n");
+        return 1;
+    }
+}
--- PolygonChecker/rectangleSolver.h
+++ PolygonChecker/rectangleSolver.h
@@ -11,3 +11,7 @@ typedef struct Point point;
 void RectangleSolver();
 void pointsView(point p1, point p2, point p3, point p4);
 int input(int n);
+int IsOrthogonal(point a, point b, point c);
+int IsRectangle(point a, point b, point c, point d);
+int ok(point a, point b, point c, point d);
+int makeSides(point p1, point p2, point p3, point p4);
