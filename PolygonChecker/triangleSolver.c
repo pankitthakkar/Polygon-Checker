4fcc178af12c4b205e12dbd2822003d3789aac90
Dhyey Vaghela <dvaghela7354@conestogac.on.ca>
2020-12-18 11:58:35 +0530

    Added the feature to print angles inside triangle

--- PolygonChecker/triangleSolver.c
+++ PolygonChecker/triangleSolver.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <math.h>  
 
 #include "triangleSolver.h"
+
+#pragma warning(disable : 4996)
+
+#define PI 3.1415926535
 
 char* analyzeTriangle(int side1, int side2, int side3) {
 
@@ -33,4 +38,58 @@ char* analyzeTriangle(int side1, int side2, int side3) {
         printf("\n%s\n\n", NotATriangle);
         return NotATriangle;
     }
+}
+
+void printAngle(int a1, int b1, int c1)
+{
+    printf("Angles of the triangle:\n");
+
+    float a = (int)(a1);
+    float b = (int)(b1);
+    float c = (int)(c1);
+
+    float alphavalue = GiveAlpha(a, b, c);
+    float betavalue = GiveBeta(a, b, c);
+    float gammavalue = GiveGamma(a, b, c);
+
+    printf("\nalpha: %f\n", alphavalue);
+    printf("beta : %f\n", betavalue);
+    printf("gamma: %f\n", gammavalue);
+}
+
+float GiveAlpha(float a, float b, float c) {
+
+    float a2 = a * a;
+    float b2 = b * b;
+    float c2 = c * c;
+
+    float alpha = acos((b2 + c2 - a2) / (2 * b * c));
+    alpha = alpha * 180 / PI;
+
+    return alpha;
+}
+
+float GiveBeta(float a, float b, float c) {
+
+    float a2 = a * a;
+    float b2 = b * b;
+    float c2 = c * c;
+
+    float beta = acos((a2 + c2 - b2) / (2 * a * c));
+
+    beta = beta * 180 / PI;
+
+    return beta;
+}
+
+float GiveGamma(float a, float b, float c) {
+
+    float a2 = a * a;
+    float b2 = b * b;
+    float c2 = c * c;
+
+    float gamma = acos((a2 + b2 - c2) / (2 * a * b));
+    gamma = gamma * 180 / PI;
+
+    return gamma;
 }
--- PolygonChecker/triangleSolver.h
+++ PolygonChecker/triangleSolver.h
@@ -1,2 +1,7 @@
 #pragma once
+#include"main.h"
 char* analyzeTriangle(int side1, int side2, int side3);
+void printAngle(int a1, int b1, int c1);
+float GiveAlpha(float a, float b, float c);
+float GiveBeta(float a, float b, float c);
+float GiveGamma(float a, float b, float c);
